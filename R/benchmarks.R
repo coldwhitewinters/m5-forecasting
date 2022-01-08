35b54941ebfc275c774ebb671a27d201f2612aa5
coldwhitewinters <51274049+coldwhitewinters@users.noreply.github.com>
2022-01-08 01:57:35 -0300

    R code for benchmarks

--- R/benchmarks.R
+++ R/benchmarks.R
@@ -2,4 +2,30 @@ library(tidyverse)
 library(fpp3)
 library(arrow)
 
-data <- read_parquet("./data/processed/base.parquet")
+
+input_files <- sprintf("./data/processed/stores/%d/data.parquet", 0:9)
+output_dir <- "./R/metrics/"
+dir.create(output_dir)
+
+acc_list <- list()
+for (i in seq_along(input_files)) {
+  data_orig <- read_parquet(input_files[[i]])
+  
+  data <- data_orig %>%
+    select(item_id, store_id, d, sales) %>% 
+    as_tsibble(key = c(item_id, store_id), index = d)
+  
+  acc <- data %>%
+    filter(d <= 1913) %>% 
+    model(NAIVE(sales)) %>% 
+    forecast(h = 28) %>%
+    accuracy(data) %>% 
+    select(-.model, -.type)
+  
+  acc_list[[i]] <- acc
+}
+
+acc_df <- reduce(acc_list, rbind)
+write_csv(acc_df, paste0(output_dir, "accuracy.csv"))
+
+df <- read_csv("./R/metrics/accuracy.csv")
