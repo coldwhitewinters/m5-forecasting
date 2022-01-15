39af250d79d897de656daea529dee6f10691b1fc
coldwhitewinters <51274049+coldwhitewinters@users.noreply.github.com>
2022-01-15 03:34:43 +0000

    Parallel training working

--- m5/models.py
+++ m5/models.py
@@ -95,39 +95,33 @@ class BottomUp:
         else:
             self.model_params = model_params
         self.regressors = regressors
-        self.n_jobs = None
+        self.n_jobs = n_jobs
 
     def train(self, **kwargs):
-        output_dir = create_dir(ROOT_DIR / f"models/{self.model_name}")
         Parallel(n_jobs=self.n_jobs)(delayed(self.train_store)(store, **kwargs) for store in range(N_STORES))
-        print("Saving model...")
-        file = output_dir / "model.pkl"
-        with open(file, "wb") as f:
-            pickle.dump(self.models_d, f)
         print("Done.")
 
     def train_store(self, store, **kwargs):
-        self.models_d = dict()
         train_data = pd.read_parquet(ROOT_DIR / f"data/processed/stores/{store}/train.parquet")
+        print(f"Training model for store {store}")
         for item in train_data.item_id.unique():
-            print(f"Training model for item {item} and store {store}   ", end="\r")
+            output_dir = create_dir(ROOT_DIR / f"models/{self.model_name}/{store}/{item}")
             train_item = train_data.loc[train_data.item_id == item, :]
             y = train_item.loc[:, "sales"].astype("float64").to_numpy()
             X = None
             if self.regressors is not None:
                 X = train_item.loc[:, self.regressors].astype("float64").to_numpy()
-            model = self.model_cls(**self.model_params)
-            self.models_d[(item, store)] = model.train(y, X, **kwargs)
-        print("\n")
+            model = self.model_cls(**self.model_params).train(y, X, **kwargs)
+            file = output_dir / "model.pkl"
+            with open(file, "wb") as f:
+                pickle.dump(model, f)
 
     def predict(self, fh, **kwargs):
         output_dir = create_dir(ROOT_DIR / f"fcst/{self.model_name}/12")
-        model_file = open(ROOT_DIR / f"models/{self.model_name}/model.pkl", "rb")
-        self.models_d = pickle.load(model_file)
-        model_file.close()
         self.fcst_l = []
-        Parallel(n_jobs=self.n_jobs)(delayed(self.predict_store)(store, **kwargs) for store in range(N_STORES))
-        print("Saving prediction...")
+        Parallel(n_jobs=self.n_jobs, require="sharedmem")(
+            delayed(self.predict_store)(store, fh, **kwargs) for store in range(N_STORES))
+        print("Saving predictions...")
         fcst_df = pd.concat(self.fcst_l)
         fcst_df.to_parquet(output_dir / "fcst.parquet")
         self.bottom_up()
@@ -135,16 +129,17 @@ class BottomUp:
 
     def predict_store(self, store, fh, **kwargs):
         val_data = pd.read_parquet(ROOT_DIR / f"data/processed/stores/{store}/val.parquet")
+        print(f"Making predictions for store {store}")
         for item in val_data.item_id.unique():
-            print(f"Making predictions for item {item} and store {store}   ", end="\r")
             val_item = val_data.loc[val_data.item_id == item, :]
             X = None
             if self.regressors is not None:
                 X = val_item.loc[:, self.regressors].astype("float64").to_numpy()
             fcst = val_item.loc[:, AGG_LEVEL[12] + ["sales"]].copy()
-            fcst["fcst"] = self.models_d[(item, store)].predict(fh, X, **kwargs)
+            with open(ROOT_DIR / f"models/{self.model_name}/{store}/{item}/model.pkl", "rb") as model_file:
+                model = pickle.load(model_file)
+            fcst["fcst"] = model.predict(fh, X, **kwargs)
             self.fcst_l.append(fcst)
-        print("\n")
 
     def bottom_up(self):
         id_cols = pd.read_parquet(ROOT_DIR / "data/processed/id-cols.parquet")
@@ -152,8 +147,7 @@ class BottomUp:
         base_fcst = base_fcst.drop(columns=["item_id", "store_id"])
         base_fcst = id_cols.join(base_fcst, how="right")
         for level in range(1, 12):
-            print(f"Making predictions for level {level}   ", end="\r")
+            print(f"Making predictions for level {level}")
             output_dir = create_dir(ROOT_DIR / f"fcst/{self.model_name}/{level}")
             fcst = base_fcst.groupby(AGG_LEVEL[level])[["sales", "fcst"]].sum().reset_index()
             fcst.to_parquet(output_dir / "fcst.parquet")
-        print("\nDone")
